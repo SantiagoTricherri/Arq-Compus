e94c698fe1dfdaaa9f35d0fcc28c1fb74a047212
delfisalinas <127854032+delfisalinas@users.noreply.github.com>
2024-06-17 09:40:31 -0300

    Update funciones_ass.h

--- funciones_ass.h
+++ funciones_ass.h
@@ -1,5 +1,5 @@
 #ifndef PROYECTO_ASSEMBLY_FUNCTIONS_H
 #define PROYECTO_ASSEMBLY_FUNCTIONS_H
-extern int luciernagas(int);
-extern int cohete(int);
+extern void luciernagas(void);
+extern void cohete(void);
 #endif //PROYECTO_ASSEMBLY_FUNCTIONS_H
