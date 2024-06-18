e77cc1a3c2d70dc498a46c68edc45607e986755f
SantiagoTricherri <165184361+SantiagoTricherri@users.noreply.github.com>
2024-06-18 00:07:49 -0300

    Update funciones_ass.h

--- funciones_ass.h
+++ funciones_ass.h
@@ -1,5 +1,6 @@
 #ifndef PROYECTO_ASSEMBLY_FUNCTIONS_H
 #define PROYECTO_ASSEMBLY_FUNCTIONS_H
-extern void luciernagas(void);
-extern void cohete(void);
-#endif //PROYECTO_ASSEMBLY_FUNCTIONS_H
+extern void cascadaDescendente(void);
+extern void parpadeoCentral(void);
+
+#endif // PROYECTO_ASSEMBLY_FUNCTIONS_H
