c6b1e7259db660d4481436b884757589e2cae3a2
SantiagoTricherri <165184361+SantiagoTricherri@users.noreply.github.com>
2024-06-17 23:42:27 -0300

    Update ass.s

--- ass.s
+++ ass.s
@@ -1,24 +1,38 @@
 .text
 
 .global cohete
+cohete:
+    PUSH {R4, R5, R6, R7, LR}
+.extern delay 
 
 .extern ledShow
 .extern disp_binary
 .extern turnOff
+.extern ledShow
 .extern delay
 .extern keyHit
 
+    MOV R5, #0x80
+    MOV R6, #0xC0
+    MOV R4, #0x0
+    MOV R7, #5
 cohete:
     PUSH {R4, R5, R6, R7, LR}
-    
+
     // Inicializar variables
     MOV R7, #10        
     MOV R5, #8         
     MOV R6, #5         
     MOV R4, #10        
 
+    MOV R0, R4
+    BL disp_binary
+    MOV R0, R4
+    BL ledShow
+    MOV R0, #10
+    BL delay
 launch:
-    
+
     MOV R3, #0        
 launch_loop:
     CMP R3, R5        
@@ -30,19 +44,19 @@ launch_loop:
     MOV R0, R0        
     BL disp_binary   
 
-   
+
     MOV R0, #3        
     MOV R1, #1        
     BL delay_wrapper
     BL check_key      
 
-    
+
     SUBS R7, R7, #500 
     ADD R3, R3, #1    
     B launch_loop     
 
 flight:
-    
+
     MOV R3, #0        
 flight_loop:
     CMP R3, R6        
@@ -53,7 +67,7 @@ flight_loop:
     MOV R0, R0        
     BL disp_binary    
 
-    
+
     MOV R0, #3        
     MOV R1, #1        
     BL delay_wrapper
@@ -64,7 +78,15 @@ flight_loop:
     MOV R0, R0        
     BL disp_binary    
 
-  
+loopC:
+    MOV R4, R6
+    MOV R0, R4
+    BL disp_binary
+    MOV R0, R4
+    BL ledShow
+    LSR R6, R6, #1
+    MOV R0, #10
+
     MOV R0, #3       
     MOV R1, #1       
     BL delay_wrapper
@@ -74,7 +96,7 @@ flight_loop:
     B flight_loop     
 
 explosion:
-    
+
     MOV R3, #0       
 explosion_loop:
     CMP R3, R4        
@@ -85,7 +107,7 @@ explosion_loop:
     MOV R0, R0        
     BL disp_binary    
 
-    
+
     MOV R0, #3        
     MOV R1, #1        
     BL delay_wrapper
@@ -96,7 +118,7 @@ explosion_loop:
     MOV R0, R0        
     BL disp_binary    
 
-    
+
     MOV R0, #3       
     MOV R1, #1        
     BL delay_wrapper
@@ -106,7 +128,7 @@ explosion_loop:
     B explosion_loop  
 
 reset_delay:
-    
+
     MOV R7, #10000
     B launch         
 
@@ -131,40 +153,36 @@ delay_wrapper:
     POP {LR}         
     MOV PC, LR       
 
-.global luciernagas
 
-.extern disp_binary
+  .global luciernagas
 .extern ledShow
-.extern delay
+.extern disp_binary
 .extern turnOff
-
+.extern delayC
 luciernagas:
-    PUSH {R4, R5, LR}          
-    LDR R4, =pattern           
-    MOV R5, #12               
+    push {lr}              
+    ldr r4, =pattern       
+    mov r5, #12            
+    mov r6, #2             
+loop_start:
+    ldrb r0, [r4]          
+    bl ledShow             
+    mov r1, r0             
+    bl disp_binary         
+    bl delay_wrapper       
+    add r4, r4, #1         
+    subs r5, r5, #1        
+    bgt loop_start         
+    bl turnOff             
+    pop {lr}               
+    mov pc, lr             
+delay_wrapper:
+    push {lr}              
+    mov r0, r6             
+    bl delayC              
+    bl delay              
+    pop {lr}               
+    mov pc, lr             
 
-loop_luciernagas:
-    LDR R6, [R4], #4           
-    BL ledShow                
-    MOV R0, R6                 
-    BL disp_binary            
-    MOV R0, #2                 
-    BL delay                  
-
-    CMP R0, #0                
-    BEQ exit_luciernagas     
-
-    SUBS R5, R5, #1            
-    BNE loop_luciernagas      
-    B loop_luciernagas        
-
-exit_luciernagas:
-    BL turnOff                
-    POP {R4, R5, PC}          
-
-.data
 pattern:
-    .word 0x00, 0x44, 0x80, 0x25, 0x60, 0x00, 0x3A, 0x91, 0x04, 0x00, 0x48, 0x00   
-
-.end
-
+    .byte 0x00, 0x44, 0x80, 0x25, 0x60, 0x00, 0x3A, 0x91, 0x04, 0x00, 0x48, 0x00
