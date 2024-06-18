c3d602f6d0eb3891b95b89b732a51c1d55fa1d58
delfisalinas <127854032+delfisalinas@users.noreply.github.com>
2024-06-18 12:25:39 -0300

    Update ass.s

--- ass.s
+++ ass.s
@@ -1,31 +1,29 @@
 .text
 
-.global carrera
-carrera:
-PUSH {R4, R5, R6, LR}
+.global luciernagas
+luciernagas:
+PUSH {R4, LR}
 
-MOV R4, #0x1 // Initialize output
-MOV R5, #8   // Loop counter
-
-loopCarrera:
+loopLuciernagas:
+BL random
+AND R4, R0, #0xFF // Mask to get a value between 0 and 255
 MOV R0, R4
 BL disp_binary
 
 MOV R0, R4
 BL ledShow
 
-LSL R4, R4, #1 // Shift left
 MOV R0, #0 // Index for delay array
 BL delay
 
 MOV R0, #0
 BL adjustSpeed // Adjust speed
 
-SUBS R5, R5, #1
-BNE loopCarrera
+CMP R0, #0
+BNE loopLuciernagas
 
 BL turnOff
-POP {R4, R5, R6, PC}
+POP {R4, PC}
 
 .global parpadeo
 parpadeo:
@@ -53,3 +51,20 @@ BNE loopParpadeo
 
 BL turnOff
 POP {R4, R5, R6, PC}
+
+.data
+random_values:
+.word 0x94A5A5A5, 0x32597A9B, 0x7B8CDE5F, 0xA5F0D7A5
+random_index:
+.word 0
+
+.global random
+random:
+    LDR R1, =random_index
+    LDR R2, [R1]
+    LDR R3, =random_values
+    LDR R0, [R3, R2, LSL #2]
+    ADD R2, R2, #1
+    AND R2, R2, #3
+    STR R2, [R1]
+    BX LR
