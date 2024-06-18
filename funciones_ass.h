719f8b63e36e518d3393245ed2152f7995a2e228
SantiagoTricherri <165184361+SantiagoTricherri@users.noreply.github.com>
2024-06-18 12:52:20 -0300

    Update funciones_ass.h

--- funciones_ass.h
+++ funciones_ass.h
@@ -1,7 +1,7 @@
 #ifndef PROYECTO_FUNCIONES_ASS_H
 #define PROYECTO_FUNCIONES_ASS_H
 
-extern void carrera(void);
+extern void luciernagas(void);
 extern void parpadeo(void);
 
 #endif //PROYECTO_FUNCIONES_ASS_H
